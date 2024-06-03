6250b6a11b9e7039d19f46393ec51df42aefb050
VictorFCh <vfariachavez@gmail.com>
2024-06-03 16:52:34 +0100

    add responsive navbar

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,11 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: "class",
 	theme: {
-		extend: {},
+		extend: {
+			animation: {
+				"appear": "appear auto linear both;"
+			},
+		},
 	},
 	plugins: [],
 }
